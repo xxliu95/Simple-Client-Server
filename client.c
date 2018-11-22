9e00fafb1e4b42373ec66741396b17e0254bdd9f
Xinxin Liu <xxliu95@gmail.com>
2018-11-22 16:02:46 +0800

    22-11-2018 Multithreaded Server

---
+++ client.c
@@ -0,0 +1,93 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+/* Format ./client ip port rate
+ * rate needs to be in KBps
+ */
+
+//#define PORT 5000 Need the user to introduce
+#define BUFFSIZE 1536
+
+int main(int argc, char *argv[]){
+
+	/* Variables */
+	int sock;
+	struct hostent *hp;
+	struct sockaddr_in server;
+	struct timeval t, start, end;
+	char buff[BUFFSIZE];
+
+	int dt1 = 0;
+	int sent = 0;
+	int startcount = 1;
+	double rate = 50000.0; //Default rate in Bps
+	long t1, t2;
+
+	if(argv[3]){
+		rate = atoi(argv[3]) * 1000;
+	}
+
+	/* Creation of the socket */
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock < 0){
+		perror("socket failed");
+		return 1;
+	}
+
+	hp = gethostbyname(argv[1]);
+	if(hp == 0){
+		perror("gethostbyname failed");
+		close(sock);
+		return 1;
+	}
+
+	server.sin_family = AF_INET;
+	memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
+	if(argv[2]){
+		server.sin_port = htons(atoi(argv[2]));
+	}else{
+		server.sin_port = htons(5000); //default port 5000
+		printf("Default port\n");
+	}
+
+	/* Connection */
+	if(connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0){
+		perror("connect failed");
+		close(sock);
+		return 1;
+	}
+
+	dt1 = sizeof(buff)*1000000/rate; // microseconds
+
+	/* Infinite loop */
+	printf("rate = %f KBps dt = %d us  buff size = %ld\n", rate/1000.0, dt1, sizeof(buff));
+
+	while(1){
+		gettimeofday(&t, NULL);
+		t1 = t.tv_sec*1000000 + t.tv_usec;
+		if(send(sock, buff, sizeof(buff), 0) < 0){
+			perror("send failed");
+			close(sock);
+			return 1;
+		}
+		while(1){
+			if(t2 >= t1 + dt1){
+				break;
+			}
+			gettimeofday(&t, NULL);
+			t2 = t.tv_sec*1000000 + t.tv_usec;
+		}
+
+	}
+	printf("Disconnected\n");
+	close(sock);
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,105 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+
+#define BUFFSIZE 15360
+#define MAXCONNECTIONS 5
+#define PORT 5000
+
+void *connection_handler(void *client_fd);
+
+int main(int argc, char *argv[]){
+
+	/* Variables */
+	int server_fd;
+	int client_fd;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	pthread_t threads[MAXCONNECTIONS];
+	int rc;
+	long t;
+
+	/* Creation of sockets */
+	server_fd = socket(AF_INET, SOCK_STREAM, 0); // ipv4 using TCP
+	if(server_fd < 0){
+		perror("Socket error");
+		return 1;
+	}
+
+	memset(&server,0,sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(PORT);
+
+	/* Bind */
+	if(bind(server_fd, (struct sockaddr *)&server, sizeof(server))){
+		perror("bind failed");
+		return 1;
+	}
+
+	/* Listen */
+	listen(server_fd, MAXCONNECTIONS);
+
+	//printf("received,rate,dt\n");
+
+	while(1){
+		pthread_t thread_id;
+		int client_len;
+
+		/* Accept */
+		client_len = sizeof(client);
+		if((client_fd = accept(server_fd,(struct sockaddr *)(&client), (socklen_t *)(&client_len))) < 0){
+			perror("accept failed");
+			return 1;
+		}
+		if(pthread_create(&thread_id,NULL,(void *)(&connection_handler),(void *)(&client_fd)) < 0){
+			perror("thread creation failed");
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/*
+ * Every thread handles one connection and
+ * calculates the rate that it receives data
+ */
+void *connection_handler(void *client_fd){
+
+	int fd = *((int *)client_fd);
+	struct timeval t1, t2;
+	char buff[BUFFSIZE];
+	double dt = 0.0;
+	double rate;
+	int timeup = 1;
+	int rcvd = 0;
+
+	while(1){
+		if(timeup){
+			gettimeofday(&t1, NULL);
+			timeup = 0;
+		}
+		int rec = recv(fd, buff, sizeof(buff), 0);
+		if(rec>0){
+			rcvd += rec; // counts received bytes
+			gettimeofday(&t2, NULL);
+			dt = ((t2.tv_sec - t1.tv_sec)*1000000.0 + (t2.tv_usec - t1.tv_usec)); // in microseconds
+		}
+		if(dt >= 1000000.0){
+			rate = rcvd*1000.0/dt;
+			printf("client = %d received = %d B rate = %5.5f KBps dt = %7.1f us.\n", fd, rcvd, rate, dt);
+			//printf("%d,%5.2f,%7.4f\n", rcvd, rate, dt);
+			rcvd = 0;
+			timeup = 1;
+		}
+	}
+	printf("Client %d Disconnected\n",fd);
+	pthread_exit(NULL);
+}
